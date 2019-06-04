2c5bb66a30386b43389abe027859f1f1d32c5a27
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-06-04 15:45:44 -0700

    Add Sampling Event page

---
+++ frontend/src/Page/SamplingEvent.elm
@@ -0,0 +1,168 @@
+module Page.SamplingEvent exposing (Model, Msg, init, toSession, update, view)
+
+import Session exposing (Session)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Page
+import Route
+import SamplingEvent exposing (SamplingEvent)
+import Sample exposing (Sample)
+import LatLng
+import Http
+--import Page.Error as Error exposing (PageLoadError)
+import Task exposing (Task)
+import String.Extra
+import Debug exposing (toString)
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { session : Session
+    , samplingEvent : Maybe SamplingEvent
+    , samples : Maybe (List Sample)
+    }
+
+
+init : Session -> Int -> ( Model, Cmd Msg )
+init session id =
+    ( { session = session
+      , samplingEvent = Nothing
+      , samples = Nothing
+      }
+      , Cmd.batch
+        [ SamplingEvent.fetch id |> Http.toTask |> Task.attempt GetSamplingEventCompleted
+        , Sample.fetchAllBySamplingEvent id |> Http.toTask |> Task.attempt GetSamplesCompleted
+        ]
+    )
+
+
+toSession : Model -> Session
+toSession model =
+    model.session
+
+
+
+-- UPDATE --
+
+
+type Msg
+    = GetSamplingEventCompleted (Result Http.Error SamplingEvent)
+    | GetSamplesCompleted (Result Http.Error (List Sample))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetSamplingEventCompleted (Ok samplingEvent) ->
+            ( { model | samplingEvent = Just samplingEvent }, Cmd.none )
+
+        GetSamplingEventCompleted (Err error) -> --TODO
+            let
+                _ = Debug.log "GetSamplingEventCompleted" (toString error)
+            in
+            ( model, Cmd.none )
+
+        GetSamplesCompleted (Ok samples) ->
+            ( { model | samples = Just samples }, Cmd.none )
+
+        GetSamplesCompleted (Err error) -> --TODO
+            let
+                _ = Debug.log "GetSamplesCompleted" (toString error)
+            in
+            ( model, Cmd.none )
+
+
+
+-- VIEW --
+
+
+view : Model -> Html Msg
+view model =
+    case model.samplingEvent of
+        Nothing ->
+            text ""
+
+        Just samplingEvent ->
+            let
+                numSamples =
+                    model.samples |> Maybe.map List.length |> Maybe.withDefault 0
+            in
+            div [ class "container" ]
+                [ Page.viewTitle (String.Extra.toSentenceCase samplingEvent.type_) samplingEvent.name
+                , div []
+                    [ viewSamplingEvent samplingEvent ]
+                , div [ class "pt-3" ]
+                    [ Page.viewTitle2 "Samples" False
+                    , span [ class "badge badge-pill badge-primary align-middle ml-2" ]
+                        [ if numSamples == 0 then
+                            text ""
+                          else
+                            text (toString numSamples)
+                        ]
+                    ]
+                , div [ class "pt-2" ]
+                    [ viewSamples model.samples ]
+                ]
+
+
+viewSamplingEvent : SamplingEvent -> Html Msg
+viewSamplingEvent samplingEvent =
+    table [ class "table table-borderless table-sm" ]
+        [ tbody []
+            [ tr []
+                [ th [] [ text "Name/ID" ]
+                , td [] [ text samplingEvent.name ]
+                ]
+            , tr []
+                [ th [] [ text (String.Extra.toSentenceCase samplingEvent.campaignType) ]
+                , td [] [ a [ Route.href (Route.Campaign samplingEvent.campaignId) ] [ text samplingEvent.campaignName ] ]
+                ]
+            , tr []
+                [ th [] [ text "Location(s)" ]
+                , td [] [ text (LatLng.formatList samplingEvent.locations) ]
+                ]
+            , tr []
+                [ th [] [ text "Start Time" ]
+                , td [] [ text samplingEvent.startTime ]
+                ]
+            , tr []
+                [ th [] [ text "End Time" ]
+                , td [] [ text samplingEvent.endTime ]
+                ]
+            , tr []
+                [ th [] [ text "Data File(s)" ]
+                , td [] [ em [] [ text "(coming soon)"] ]
+                ]
+            ]
+        ]
+
+
+viewSamples : Maybe (List Sample) -> Html Msg
+viewSamples maybeSamples =
+    let
+        mkRow sample =
+            tr []
+                [ td [ style "white-space" "nowrap" ]
+                    [ a [ Route.href (Route.Sample sample.id) ] [ text sample.accn ] ]
+                ]
+
+        sortByAccn a b =
+            compare a.accn b.accn
+    in
+    case maybeSamples of
+        Nothing ->
+            text "None"
+
+        Just samples ->
+            table [ class "table" ]
+                [ thead []
+                    [ tr []
+                        [ th [] [ text "Accn" ]
+                        ]
+                    ]
+                , tbody []
+                    (samples |> List.sortWith sortByAccn |> List.map mkRow)
+                ]
---
+++ frontend/src/SamplingEvent.elm
@@ -0,0 +1,74 @@
+module SamplingEvent exposing (SamplingEvent, fetch, fetchAllByCampaign)
+
+{-| The interface to the Sample data structure.
+-}
+
+import Http
+import HttpBuilder
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (required, optional)
+import Json.Encode as Encode
+import LatLng exposing (LatLng)
+import Config exposing (apiBaseUrl)
+import Debug exposing (toString)
+
+
+
+-- TYPES
+
+
+type alias SamplingEvent  =
+    { id : Int
+    , type_ : String
+    , name : String
+    , locations : List LatLng
+    , startTime : String
+    , endTime : String
+    , campaignId : Int
+    , campaignType : String
+    , campaignName : String
+    }
+
+
+
+-- SERIALIZATION
+
+
+samplingEventDecoder : Decoder SamplingEvent
+samplingEventDecoder =
+    Decode.succeed SamplingEvent
+        |> required "sampling_event_id" Decode.int
+        |> required "sampling_event_type" Decode.string
+        |> required "name" Decode.string
+        |> required "locations" (Decode.list LatLng.decoder)
+        |> optional "start_time" Decode.string ""
+        |> optional "end_time" Decode.string ""
+        |> optional "campaign_id" Decode.int 0
+        |> optional "campaign_type" Decode.string ""
+        |> optional "campaign_name" Decode.string ""
+
+
+
+-- REQUESTS
+
+
+fetch : Int -> Http.Request SamplingEvent
+fetch id  =
+    let
+        url =
+            apiBaseUrl ++ "/sampling_events/" ++ (toString id)
+    in
+    HttpBuilder.get url
+        |> HttpBuilder.withExpect (Http.expectJson samplingEventDecoder)
+        |> HttpBuilder.toRequest
+
+
+fetchAllByCampaign : Int -> Http.Request (List SamplingEvent)
+fetchAllByCampaign id =
+    let
+        url =
+            apiBaseUrl ++ "/campaigns/" ++ (toString id) ++ "/sampling_events"
+    in
+    HttpBuilder.get url
+        |> HttpBuilder.withExpect (Http.expectJson (Decode.list samplingEventDecoder))
+        |> HttpBuilder.toRequest
