f98f63235cbc52cbbd73fe4d61e2b2a82422fae8
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-06-11 19:47:23 -0700

    Move index.html/js into src

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-import './assets/scss/bootstrap_custom.scss'
-import './assets/css/landing-page.css';
-import './assets/css/loader.css';
-//import './assets/css/elm-datepicker.css';
+import '../assets/scss/bootstrap_custom.scss'
+import '../assets/css/landing-page.css';
+import '../assets/css/loader.css';
+//import '../assets/css/elm-datepicker.css';
 import $ from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 import "@fortawesome/fontawesome-free/js/all.min.js"
-import { Elm } from './src/Main.elm';
+import { Elm } from './Main.elm';
 
 
 var app = Elm.Main.init({
