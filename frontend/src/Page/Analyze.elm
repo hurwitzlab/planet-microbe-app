fcbd9b7245eb87921895b0482f400c3635e95172
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-08-01 18:21:21 -0700

    Show login message for jobs if not logged in

--- frontend/src/Page/Analyze.elm
+++ frontend/src/Page/Analyze.elm
@@ -1,6 +1,6 @@
 module Page.Analyze exposing (Model, Msg, init, toSession, update, view)
 
-import Session exposing (Session)
+import Session exposing (Session(..))
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Page
@@ -21,20 +21,33 @@ import Agave exposing (Job)
 type alias Model =
     { session : Session
     , apps : Maybe (List App)
-    , jobs : Maybe (List Job)
+    , jobs : Jobs
     }
+
+
+type Jobs
+    = Unavailable
+    | Loading
+    | Loaded (List Job)
+    | LoadError
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
+    let
+        (jobs, getJobs) =
+            case session of
+                LoggedIn _ _ cred ->
+                    ( Loading, [ Agave.getJobs cred.token |> Http.toTask |> Task.map .result |> Task.attempt GetJobsCompleted ] )
+
+                _ ->
+                    ( Unavailable, [] )
+    in
     ( { session = session
       , apps = Nothing
-      , jobs = Nothing
+      , jobs = jobs
       }
-      , Cmd.batch
-        [ App.fetchAll |> Http.toTask |> Task.attempt GetAppsCompleted
-        , Agave.getJobs (Session.token session) |> Http.toTask |> Task.map .result |> Task.attempt GetJobsCompleted
-        ]
+      , Cmd.batch ( (App.fetchAll |> Http.toTask |> Task.attempt GetAppsCompleted) :: getJobs )
     )
 
 
@@ -65,13 +78,13 @@ update msg model =
             ( model, Cmd.none )
 
         GetJobsCompleted (Ok jobs) ->
-            ( { model | jobs = Just jobs }, Cmd.none )
+            ( { model | jobs = Loaded jobs }, Cmd.none )
 
         GetJobsCompleted (Err error) -> --TODO
 --            let
 --                _ = Debug.log "GetJobsCompleted" (toString error)
 --            in
-            ( model, Cmd.none )
+            ( { model | jobs = LoadError } , Cmd.none )
 
 
 
@@ -81,56 +94,59 @@ update msg model =
 view : Model -> Html Msg
 view model =
     let
+        numApps =
+            model.apps |> Maybe.withDefault [] |> List.length
+
         appsRow =
             case model.apps of
                 Just apps ->
-                    let
-                        numApps =
-                            List.length apps
-                    in
-                    div []
-                        [ div [ class "pt-5" ]
-                            [ Page.viewTitle2 "Apps" False
-                            , span [ class "badge badge-pill badge-primary align-middle ml-2" ]
-                                [ if numApps == 0 then
-                                    text ""
-                                  else
-                                    text (String.fromInt numApps)
-                                ]
-                            ]
-                        , div []
-                            [ viewApps apps ]
-                        ]
+                    viewApps apps
 
                 Nothing ->
                     Page.viewSpinner
 
-        jobsRow =
+        ( numJobs, jobsRow ) =
             case model.jobs of
-                Just jobs  ->
-                    let
-                        numJobs =
-                            List.length jobs
-                    in
-                    div []
-                        [ div [ class "pt-3" ]
-                            [ Page.viewTitle2 "Jobs" False
-                            , span [ class "badge badge-pill badge-primary align-middle ml-2" ]
-                                [ if numJobs == 0 then
-                                    text ""
-                                  else
-                                    text (String.fromInt numJobs)
-                                ]
-                            ]
-                        , div [ class "pt-2", style "overflow-y" "auto", style "max-height" "80vh" ]
-                            [ viewJobs jobs ]
+                Unavailable ->
+                    ( 0
+                    , div [ class "pt-2" ]
+                        [ a [ Route.href Route.Login ] [ text "Sign-in" ]
+                        , text " to see your jobs"
                         ]
+                    )
 
-                Nothing ->
-                    Page.viewSpinner
+                Loading ->
+                    ( 0, Page.viewSpinner )
+
+                Loaded jobs  ->
+                    ( List.length jobs
+                    , div [ style "overflow-y" "auto", style "max-height" "80vh" ]
+                        [ viewJobs jobs ]
+                    )
+
+                LoadError ->
+                    ( 0, div [ class "pt-2" ] [ text "An error occured" ] )
     in
     div [ class "container" ]
-        [ appsRow
+        [ div [ class "border-bottom pt-5" ]
+            [ Page.viewTitle2 "Apps" False
+            , span [ class "badge badge-pill badge-primary align-middle ml-2" ]
+                [ if numApps == 0 then
+                    text ""
+                  else
+                    text (String.fromInt numApps)
+                ]
+            ]
+        , appsRow
+        , div [ class "border-bottom pt-4" ]
+            [ Page.viewTitle2 "Jobs" False
+            , span [ class "badge badge-pill badge-primary align-middle ml-2" ]
+                [ if numJobs == 0 then
+                    text ""
+                  else
+                    text (String.fromInt numJobs)
+                ]
+            ]
         , jobsRow
         ]
 
@@ -165,8 +181,8 @@ viewJobs jobs =
             tr []
                 [ td [] [ a [ Route.href (Route.Job job.id)] [ text job.name ] ]
                 , td [] [ text "" ]
-                , td [] [ text "" ]
-                , td [] [ text "" ]
+                , td [] [ text job.created ]
+                , td [] [ text job.ended ]
                 , td [] [ text job.status ]
                 ]
     in
