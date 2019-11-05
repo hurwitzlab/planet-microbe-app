f409be7474aafbf44a5484fefc6057ad54c09256
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-11-05 16:09:17 -0700

    Tweak chart styling

--- frontend/src/BarChart.elm
+++ frontend/src/BarChart.elm
@@ -18,6 +18,7 @@ type alias Config a =
     , padding : Float
     , formatter : (a -> String)
     , title : Maybe String
+    , maxLabelLen : Int
     }
 
 
@@ -28,7 +29,13 @@ defaultConfig =
     , padding = 30
     , formatter = (\s -> s)
     , title = Nothing
+    , maxLabelLen = 25
     }
+
+
+titleHeight : Float
+titleHeight
+    = 17
 
 
 xScale : Float -> Float -> List ( a, Float ) -> BandScale a
@@ -42,9 +49,17 @@ yScale h padding range =
     Scale.linear ( h - 2 * padding, 0 ) range
 
 
-xAxis : Float -> Float -> List ( a, Float ) -> (a -> String) -> Svg msg
-xAxis w padding model formatter =
-    Axis.bottom [] (Scale.toRenderable formatter (xScale w padding model))
+xAxis : Float -> Float -> Int -> List ( a, Float ) -> (a -> String) -> Svg msg
+xAxis w padding maxLabelLen model formatter =
+    Axis.bottom [] (Scale.toRenderable (formatter >> truncate maxLabelLen) (xScale w padding model))
+
+
+truncate : Int -> String -> String
+truncate len s =
+    if String.length s > len then
+        (String.slice 0 (len - 3) s) ++ "..."
+    else
+        s
 
 
 yAxis : Float -> Float -> (Float, Float) -> Svg msg
@@ -64,7 +79,7 @@ column w h padding range formatter scale ( label, value ) =
             []
         , text_
             [ x <| Scale.convert (Scale.toRenderable formatter scale) label
-            , y <| Scale.convert (yScale h padding range) value - 5
+            , y <| Scale.convert (yScale h padding range) value - 2
             , textAnchor AnchorMiddle
             ]
             [ text <| (String.fromFloat value) ]
@@ -86,32 +101,33 @@ view config model =
             )
 
         maxLabelLen =
-            model |> List.map (Tuple.first >> config.formatter >> String.length) |> List.maximum |> Maybe.withDefault 0
+            model |> List.map (Tuple.first >> config.formatter >> truncate config.maxLabelLen >> String.length) |> List.maximum |> Maybe.withDefault 0
 
         padding =
             config.padding
 
+        chartWidth =
+            config.width - (toFloat maxLabelLen) * 1.5 -- account for x-axis labels width
+
         chartHeight =
-            config.height - 2 * config.padding - (toFloat maxLabelLen) * 1.5
+            config.height - 2 * config.padding - (toFloat maxLabelLen) * 1.5 -- account for x-axis labels height
     in
     svg [ width config.width, height config.height] --[ viewBox 0 0 config.width config.height ]
         [ style [] [ text """
             .column rect { fill: #00bbcc; }
-            .column text { display: none; }
-            .column:hover rect { fill:  #008d97; }
-            .column:hover text { display: inline; }
+            .column text { display: inline; font-size: 0.65em; }
             .xaxis .tick text { writing-mode: tb; text-anchor: start; transform:translate(-12px,0px) rotate(-45deg);" }
           """ ]
         , case config.title of
             Just title ->
-                text_ [ textAnchor AnchorMiddle, TypedSvg.Attributes.x (Percent 50), y (config.padding - 5) ] [ text title ]
+                text_ [ textAnchor AnchorMiddle, TypedSvg.Attributes.x (Percent 50), y (config.padding - titleHeight) ] [ text title ]
 
             Nothing ->
                 g [] []
         , g [ transform [ Translate (padding - 1) (chartHeight - padding) ], class [ "xaxis" ] ]
-            [ xAxis config.width padding model config.formatter ]
+            [ xAxis chartWidth padding config.maxLabelLen model config.formatter ]
         , g [ transform [ Translate padding padding ] ]
             [ yAxis chartHeight padding range ]
         , g [ transform [ Translate padding padding ], class [ "series" ] ] <|
-            List.map (column config.width chartHeight padding range config.formatter (xScale config.width padding model)) model
+            List.map (column chartWidth chartHeight padding range config.formatter (xScale chartWidth padding model)) model
         ]
