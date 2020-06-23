d88b3931ba8b7b624be8a728e6ac5a790bf3b8a5
Matt Bomhoff <mbomhoff@email.arizona.edu>
2020-06-23 16:04:48 -0700

    Add project/sample tests

--- backend/__tests__/search.js
+++ backend/__tests__/search.js
@@ -5,29 +5,29 @@ const apiBaseUrl = `http://localhost:${config.serverPort}`;
 const get = route => requestp({ method: 'GET', uri: `${apiBaseUrl}/${route}`, json: true });
 const post = (route, data) => requestp({ method: 'POST', uri: `${apiBaseUrl}/${route}`, body: data, json: true });
 
+
+/*
+ * Search
+ *   - all tests were written so to not be invalidated by adding new datasets
+ */
+
 const getSearchTerm = id => get(`searchTerms/${id}`);
 const search = constraints => post(`search`, constraints);
 
-
-/*
-NOTES:
-    - these tests are not affected by adding new datasets
-*/
-
-it('should return search term descriptor for "depth of water"', async () => {
+it('search: should return search term descriptor for "depth of water"', async () => {
     const data = await getSearchTerm('http://purl.obolibrary.org/obo/ENVO_3100031');
     expect(data).toBeDefined();
     expect(data.length).toEqual(1);
     expect(data[0].id).toEqual('http://purl.obolibrary.org/obo/ENVO_3100031');
 });
 
-it('should return search results for project filter', async () => {
+it('search: should return search results for project filter', async () => {
     const data = await search({ 'project': 'Tara Oceans' });
     expect(data).toBeDefined();
     expect(data.sampleCount).toEqual(1250)
 });
 
-it('should return search results for location filter', async () => {
+it('search: should return search results for location filter', async () => {
     const data = await search({
         'project': 'Tara Oceans',
         'location': '[35,-36,1400]'
@@ -36,7 +36,7 @@ it('should return search results for location filter', async () => {
     expect(data.sampleCount).toEqual(33)
 });
 
-it('should return search results for depth filter', async () => {
+it('search: should return search results for depth filter', async () => {
     const data = await search({
         'project': 'GOS',
         'http://purl.obolibrary.org/obo/ENVO_3100031': '[0,10]' //depth
@@ -45,7 +45,7 @@ it('should return search results for depth filter', async () => {
     expect(data.sampleCount).toEqual(41)
 });
 
-it('should return search results for date filter', async () => {
+it('search: should return search results for date filter', async () => {
     const data = await search({
         'project': 'Tara Oceans',
         '|http://purl.obolibrary.org/obo/OBI_0001619': '[2010-01-01,2010-06-01]',  // date
@@ -56,7 +56,7 @@ it('should return search results for date filter', async () => {
     expect(data.sampleCount).toEqual(216)
 });
 
-it('should return search results for string filter', async () => {
+it('search: should return search results for string filter', async () => {
     const data = await search({
         'project': 'OSD',
         'http://purl.obolibrary.org/obo/ENVO_00000428': 'http://purl.obolibrary.org/obo/envo_00000447' // biome = "Marine biome"
@@ -65,7 +65,7 @@ it('should return search results for string filter', async () => {
     expect(data.sampleCount).toEqual(93)
 });
 
-it('should return sorted search results for numeric range filter', async () => {
+it('search: should return sorted search results for numeric range filter', async () => {
     const data = await search({
         'project': 'HOT ALOHA time/depth series',
         'http://purl.obolibrary.org/obo/ENVO_09200014': '[0,10]', // temperature of water
@@ -79,7 +79,7 @@ it('should return sorted search results for numeric range filter', async () => {
     expect(data.sampleResults[0].values[0]).toEqual([3.7316, 3.72, 3.72, 3.72]);
 });
 
-it('should return sorted search results for bunch of filters', async () => {
+it('search: should return sorted search results for bunch of filters', async () => {
     const data = await search({
         'project': "Amazon Plume Metagenomes|Amazon Plume Metatranscriptomes|Amazon PolyA Metatranscriptomes|Amazon River Metagenomes|Amazon River Metatranscriptomes|BATS Chisholm|CDEBI Juan de Fuca Ridge Flank|GOS|HOT ALOHA time/depth series|HOT Chisholm|HOT DeLong|HOT DeLong Metatranscriptomes|OSD|Tara Oceans|Tara Polar Circle Expedition",
         'location': "[46,2.7,2265]",
@@ -99,3 +99,43 @@ it('should return sorted search results for bunch of filters', async () => {
     expect(data.sampleResults[0]).toBeDefined();
     expect(data.sampleResults[0].sampleAccn).toEqual('SAMEA3275534');
 });
+
+
+/*
+ * Projects
+ */
+
+const getProject = id => get(`projects/${id}`);
+
+it('projects: should return all projects', async () => {
+    const projects = await getProject('');
+    expect(projects).toBeDefined();
+    expect(projects.length).toBeGreaterThanOrEqual(15);
+
+    for (const project of projects) {
+        const data = await getProject(project.project_id);
+        expect(data).toBeDefined();
+        expect(data.datapackage_url).toBeDefined();
+        expect(data.sample_count).toBeGreaterThan(0);
+    }
+});
+
+
+/*
+ * Samples
+ */
+
+const getSamples = () => post('samples', {});
+const getSample = id => get(`samples/${id}`);
+
+it('samples: should return all samples', async () => {
+    const samples = await getSamples();
+    expect(samples).toBeDefined();
+    expect(samples.length).toBeGreaterThanOrEqual(2371);
+
+    for (const sample of samples) {
+        const data = await getSample(sample.sample_id);
+        expect(data).toBeDefined();
+    }
+});
+
